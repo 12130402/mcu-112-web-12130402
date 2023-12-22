04b5b7cb7e6fa074e031423fa2f6bc44b6765b7b
12130402 <12130402@me.mcu.edu.tw>
2023-12-22 16:33:00 +0800

    feat(todo-list): 無待辦事項顯示實作

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoComponent } from '../todo/todo.component';
@@ -7,7 +7,7 @@ import { Todo } from '../model/todo';
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [NgFor, TodoComponent],
+  imports: [NgIf, NgFor, TodoComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css',
 })
